c50b9bc1b6f0026ceef12bad4d38c49f27cde83b
JennieChu713 <38804711+JennieChu713@users.noreply.github.com>
2022-01-19 10:50:14 +0800

    feat: created resume with vanilla js.

---
+++ index.js
@@ -0,0 +1,32 @@
+// interactive elements
+const photo = document.getElementsByClassName("contact__avatar")[0];
+const projects = document.getElementsByClassName("right-side__portfolios")[0];
+
+// change effect functions
+function photoShowToggle(e) {
+  const triggerElements = ["figcaption", "img", "div"];
+  const { localName } = e.target;
+  if (triggerElements.indexOf(localName) > -1) {
+    const [figcaption, img, div] = triggerElements;
+    photo.querySelector(figcaption).classList.toggle("captionHide");
+    photo.querySelector(div).classList.toggle("clearShade");
+    photo.querySelector(img).classList.toggle("showPhoto");
+  }
+}
+
+function projectDisplayContent(e) {
+  if (e.target.classList.contains("intro__toggle-btn")) {
+    e.target.nextElementSibling.classList.toggle("showIntro");
+    e.target.nextElementSibling.classList.toggle("hideIntro");
+
+    if (e.target.innerText === "詳細") {
+      e.target.innerText = "收起";
+    } else {
+      e.target.innerText = "詳細";
+    }
+  }
+}
+
+// eventListeners
+photo.addEventListener("click", photoShowToggle, false);
+projects.addEventListener("click", projectDisplayContent, false);
